6d0aed1150762413d17b202d9957d467059ba0a6
Yaes2002 <deanhendriks2002@gmail.com>
2020-11-16 16:13:54 +0100

    start

---
+++ New Unity Project/Assets/Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+   public float MovementSpeed = 1;
+   public float Jumpforce = 1;
+
+   private Rigidbody2D _rigidbody;
+
+   private void Start()
+   {
+       _rigidbody = GetComponent<Rigidbody2D>();
+   }
+
+   private void Update()
+   {
+       var movement = Input.GetAxis("Horizontal");
+       transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+       if (!Mathf.Approximately(0, movement))
+            transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+   
+       if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.0001f)
+       {
+           _rigidbody.AddForce(new Vector2(0, Jumpforce), ForceMode2D.Impulse);
+       }
+   }
+
+}
